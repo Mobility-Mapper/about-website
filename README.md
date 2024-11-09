40481649d2727ca97e4dcfd7d5f0e08d3ed02ed7
SonyaJane <sonya.ridden@gmail.com>
2024-11-09 22:38:16 +0000

    Added subsribeform response to modal with close window button

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 The introduction text is positioned below the hero imahe on small screens, and moves up onto it for medium screens and above.
+
+The subscribe form posts the name and email to a Google spreadsheet. This is done by the subscribe.js script.
---
+++ assets/js/subscribe1.js
@@ -0,0 +1,29 @@
+document.getElementById('subscribe-form').addEventListener('submit', async function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+
+    const responseMessage = document.getElementById('response-message');
+    const modalBody = document.querySelector('.modal-body');
+
+    const response = await fetch("https://script.google.com/macros/s/AKfycbxvia1M4cVNhTOoRg4ABBBNU_4e_b0DrKk4R_55ZPPrZhxxJg11jFU5v6v0YIODEnHR/exec", {
+        method: "POST",
+        body: JSON.stringify({ name, email })
+    });
+
+    if (response.ok) {
+
+        // Display the success message
+        responseMessage.innerHTML = `
+        <p class="text-success p-responsive m-3">Thank you for subscribing!</p>
+        <button type="button" class="btn m-3 modal-button" data-bs-dismiss="modal">
+            Close window 
+        </button>`;
+
+        // Hide the modal body
+        modalBody.style.display = 'none';
+
+    } else {
+        responseMessage.innerHTML = '<p class="text-danger">Oops! Something went wrong. Please try again.</p>';
+    }
+});
